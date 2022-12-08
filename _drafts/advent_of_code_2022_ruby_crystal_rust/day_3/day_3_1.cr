58264553871b6e65ed4bc58a8e447f49f40fab26
Kirk Haines <wyhaines@gmail.com>
2022-12-08 07:27:20 -0700

    Add the Ruby and Crystal examples for Day 3 part 1.

---
+++ _drafts/advent_of_code_2022_ruby_crystal_rust/day_3/day_3_1.cr
@@ -0,0 +1,41 @@
+class RuckSackn
+  def initialize(filename)
+    rucksacks = parse_rucksacks(filename)
+    sum = rucksacks.map { |rucksack| item_priority(find_duplicates(*rucksack)) }.sum
+
+    puts "Priority sum of all duplicated items: #{sum.inspect}"
+  end
+
+  def parse_rucksacks(filename)
+    File.read(filename).split("\n").map do |line|
+      middle = line.size // 2
+      left = line[0..(middle - 1)]
+      right = line[middle..]
+      {left, right}
+    end
+  end
+
+  # The instructions are ambiguous about whether or not there may be more than one duplicate,
+  # so this code and the code in `item_priority` can handle any number of duplicates.
+  def find_duplicates(left, right)
+    left.split("") & right.split("")
+  end
+
+  def item_priority(items)
+    items.reduce(0) do |sum, item|
+      character = item[0]?
+      if character
+        case character
+        when ('a'..'z')
+          sum += character.ord - 96
+        when ('A'..'Z')
+          sum += character.ord - 38
+        end
+      end
+
+      sum
+    end
+  end
+end
+
+RuckSackn.new(ARGV[0]? || "input.txt")
